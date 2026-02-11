f40b3dee35015da6d16d42260c61ec9c96d7e67d
cristhi21 <cristhian.avc21@gmail.com>
2026-02-10 23:20:56 -0500

    Refactoring and Add documentation

--- Modulo_1/readingMessagesBySockets/src/main/java/org/jave/modulo1/firstsolution/Constants.java
+++ Modulo_1/readingMessagesBySockets/src/main/java/org/jave/modulo1/firstsolution/Constants.java
@@ -7,5 +7,6 @@ public class Constants {
 
     public static final int BUFFER_SIZE = 1024;
     public static final int TIMEOUT = 5000;
-    public static final int THREAD_SLEEP = 5000;
+    public static final int THREAD_SLEEP = 1000;
+    public static final int NUMBER_OF_MESSAGES = 20;
 }
--- Modulo_1/readingMessagesBySockets/src/main/java/org/jave/modulo1/firstsolution/Server.java
+++ Modulo_1/readingMessagesBySockets/src/main/java/org/jave/modulo1/firstsolution/Server.java
@@ -4,67 +4,90 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Servidor TCP que recibe mensajes de un cliente, responde con "OK" y mide el tiempo de procesamiento de cada mensaje.
+ * El servidor se cierra automáticamente después de recibir un número específico de mensajes.
+ *
+ * @autor grupo3
+ */
 public class Server {
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
     private DataInputStream in;
+    private DataOutputStream out;
     private final ServerSocket serverSocket;
 
     public Server(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
 
-    private void startServer() {
+    /**
+     * Inicia el servidor, acepta conexiones de clientes y procesa los mensajes recibidos. El servidor se cerrará automáticamente
+     * después de recibir un número específico de mensajes, según lo definido en Constants.NUMBER_OF_MESSAGES.
+     */
+    private void startServer() throws IOException {
         logger.info("Servidor ha Iniciado...");
-        try {
-            Socket socketClient = this.serverSocket.accept();
-            in = new DataInputStream(socketClient.getInputStream());
-            logger.info("Se ha conectado el cliente = {}", socketClient);
+        Socket socketClient = this.serverSocket.accept();
+        in = new DataInputStream(socketClient.getInputStream());
+        out = new DataOutputStream(socketClient.getOutputStream());
+        logger.info("Se ha conectado el cliente = {}", socketClient);
 
-            while (!this.serverSocket.isClosed()) {
-                gettingMessage(socketClient);
+        while (!this.serverSocket.isClosed()) {
+            String gotMessage = getMessageFromClientAndCalculateTime();
+            if (isTheLastMessage(gotMessage)) {
+                logger.info("Se han enviado todos los mensajes, cerrando conexión...");
+                closeConnectionServer(socketClient);
+                break;
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Cierra la conexión con el cliente y registra que el usuario ha abandonado.
+     *
+     * @param client
+     * @throws IOException
+     */
     private void closeConnectionServer(Socket client) throws IOException {
         client.close();
         logger.info("El usuario abandono");
     }
 
-    private void gettingMessage(Socket client) {
-        try {
-            // --- Bloque de código a medir ---
-            String receivedMessage = getMessageFromClientAndCalculateTime();
-            //
-            if (receivedMessage.equals("100")) {
-                logger.info("La conexión se cerrara, gracias");
-                closeConnectionServer(client);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+    /**
+     * Recibe un mensaje del cliente, responde con "OK" y mide el tiempo de procesamiento del mensaje. El tiempo se registra en milisegundos.
+     *
+     * @return
+     * @throws IOException
+     */
     private String getMessageFromClientAndCalculateTime() throws IOException {
-        // Capturar tiempo inicial
-        long inicioNano = System.nanoTime();
+        long inicioNano = System.nanoTime(); // Capturar tiempo inicial
         String receivedMessage = in.readUTF(); // Recibir mensaje
-        // Capturar tiempo final
-        long finNano = System.nanoTime();
-        // Calcular diferencias
-        long duracionNano = finNano - inicioNano;
-        double duracionMili = (double) duracionNano / Constants.MILISEGUNDOS_CONVERSOR;
-
-        logger.info("Mensaje {} recibido - Tiempo en milisegundos: {} ms", receivedMessage, duracionMili);
+        out.writeUTF("OK"); //Respuesta al cliente
+        long finNano = System.nanoTime(); // Capturar tiempo final
+        long duracionNano = finNano - inicioNano; // Calcular diferencias
+        //double duracionMili = (double) duracionNano / Constants.MILISEGUNDOS_CONVERSOR;
+        //logger.info("Mensaje {} recibido - Tiempo en milisegundos: {} ms", receivedMessage, duracionMili);
         return receivedMessage;
     }
 
+    /**
+     * Verifica si el mensaje recibido es el último mensaje esperado, según lo definido en Constants.NUMBER_OF_MESSAGES.
+     *
+     * @param gotMessage
+     * @return
+     */
+    private static boolean isTheLastMessage(String gotMessage) {
+        return gotMessage.equals(String.valueOf(Constants.NUMBER_OF_MESSAGES));
+    }
+
+    /**
+     * Punto de entrada del programa. Inicia el servidor TCP en el puerto definido en Constants.SERVER_PORT.
+     *
+     */
     public static void main(String[] args) {
         try {
             Server server = new Server(new ServerSocket(Constants.SERVER_PORT));
